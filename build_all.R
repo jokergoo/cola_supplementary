d87e3dfc3b8005196065260901877f373589d044
jokergoo <jokergoo@gmail.com>
2020-02-10 11:16:52 +0100

    update

---
+++ build_all.R
@@ -0,0 +1,8 @@
+
+library(GetoptLong)
+library(bsub)
+rmd_files = scan(pipe("ls $PWD/*/*.Rmd"), what = "character")
+for(f in rmd_files) {
+	cmd = qq("module load R/3.6.0; module load pandoc/2.2.1; Rscript -e 'rmarkdown::render(\"@{f}\")'")
+	bsub_cmd(cmd, name = qq("cola_@{gsub('.Rmd$', '', basename(f))}"), hour = 5, memory = 5)
+}
